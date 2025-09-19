f5c2fad31cd0ea8c5fc1ef30861c460d6027f0f0
yogeshg45 <151724436+yogeshg45@users.noreply.github.com>
2025-09-19 19:33:05 +0530

    app.py

--- app.py
+++ app.py
@@ -329,4 +329,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # fallback to Render default port 10000
+    app.run(host="0.0.0.0", port=port)
+
